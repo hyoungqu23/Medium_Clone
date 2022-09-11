b5e6ca0df095ae46c4ad807b7e7f9649ff6056c4
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-09-12 01:12:53 +0900

    Feat: Post Page의 기본 UI 구현

--- frontend/pages/post/[slug].tsx
+++ frontend/pages/post/[slug].tsx
@@ -7,12 +7,36 @@ interface Props {
   post: Post;
 }
 
-const Post = (props: Props) => {
-  console.log(props.post);
-
+const Post = ({ post }: Props) => {
   return (
     <main>
       <Header />
+
+      <img
+        className="w-full h-40 object-cover"
+        src={urlFor(post.mainImage).url()!}
+        alt="Post Image"
+      />
+
+      <article className="max-w-3xl mx-auto p-5">
+        <h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
+        <h2 className="text-xl font-light text-gray-500 mb-2">
+          {post.description}
+        </h2>
+
+        <div className="flex items-center space-x-2">
+          <img
+            className="h-10 w-10 rounded-full "
+            src={urlFor(post.author.image).url()!}
+            alt="Author Image"
+          />
+          <p className="font-extralight text-sm">
+            Blog Post by{' '}
+            <span className="text-green-600">{post.author.name}</span> -
+            Published at {new Date(post._createdAt).toLocaleString()}
+          </p>
+        </div>
+      </article>
     </main>
   );
 };
