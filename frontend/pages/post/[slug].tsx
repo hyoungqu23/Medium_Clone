362a3c622e5ee15b263fd0e01b0e761e887514b0
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-09-12 09:03:12 +0900

    Feat: Comment Form UI 구현

--- frontend/pages/post/[slug].tsx
+++ frontend/pages/post/[slug].tsx
@@ -3,12 +3,38 @@ import Header from '../../components/Header';
 import { sanityClient, urlFor } from '../../sanity';
 import { Post } from '../../typing';
 import PortableText from 'react-portable-text';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
+interface IFormInput {
+  _id: string;
+  name: string;
+  email: string;
+  comment: string;
+}
 interface Props {
   post: Post;
 }
 
 const Post = ({ post }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>();
+
+  const handleInputSubmit: SubmitHandler<IFormInput> = (data) => {
+    fetch('/api/createComment', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    })
+      .then(() => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <main>
       <Header />
@@ -73,7 +99,61 @@ const Post = ({ post }: Props) => {
         </div>
       </article>
 
-      <hr className="max-w-lg my-5 mx-auto border border-green-500" />
+      <hr className="max-w-lg my-5 mx-auto border border-yellow-500" />
+
+      <form
+        className="flex flex-col p-5 max-w-2xl mx-auto mb-10"
+        onSubmit={handleSubmit(handleInputSubmit)}
+      >
+        <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
+        <h4 className="text-3xl font-bold">Leave a comment below!</h4>
+        <hr className="py-3 mt-2" />
+
+        <input {...register('_id')} type="hidden" name="_id" value={post._id} />
+
+        <label className="block mb-5" htmlFor="">
+          <span className="text-gray-700">Name</span>
+          {errors.name && (
+            <span className="text-red-500"> - Name Field is required</span>
+          )}
+          <input
+            {...register('name', { required: true })}
+            className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+            type="text"
+            placeholder="Pete Mitchell"
+          />
+        </label>
+        <label className="block mb-5" htmlFor="">
+          <span className="text-gray-700">Email</span>
+          {errors.email && (
+            <span className="text-red-500"> - Email Field is required</span>
+          )}
+          <input
+            {...register('email', { required: true })}
+            className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+            type="email"
+            placeholder="Pete Mitchell"
+          />
+        </label>
+        <label className="block mb-5" htmlFor="">
+          <span className="text-gray-700">Comment</span>
+          {errors.comment && (
+            <span className="text-red-500"> - Comment Field is required</span>
+          )}
+          <textarea
+            {...register('comment', { required: true })}
+            className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+            placeholder="Pete Mitchell"
+            rows={8}
+          />
+        </label>
+
+        <input
+          className="shadow bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer"
+          type="submit"
+          value="Submit"
+        />
+      </form>
     </main>
   );
 };
